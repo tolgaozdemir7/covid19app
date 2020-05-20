8cc8604a3d6a531bd846266b4367b2863469329d
Tolga Özdemir <ozdemit@yahoo.com>
2020-05-20 10:16:29 +0300

    Delete covid_stats_provider.dart

--- lib/model/covid_stats_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:covid19app/model/weekly_covid_stats.dart';
-
-class CovidStatsProvider {
-  WeeklyCovidStats weeklyStats;
-
-  Future<void> load({String city = 'Turkey'}) async {
-    await weeklyStats.loadData();
-  }
-}
